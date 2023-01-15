b5fe88aeb1f0afdb9be9ae3220f6eba3677ae391
dvg059 <dom.vag@gmail.com>
2023-01-15 09:23:25 -0500

    file reorgination

---
+++ utest/json_unit_test.cpp
@@ -0,0 +1,19 @@
+
+#include "../include/CBSTTree.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+// The fixture for testing class leetcode_bsttree.
+TEST(solution, countBits) {
+
+} // end test
+
+}  // namespace
+
+/********************************************/
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
